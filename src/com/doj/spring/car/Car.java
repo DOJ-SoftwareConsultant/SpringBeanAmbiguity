f021656fa6bf275cac125df2ae877c51a23bca12
Sumit <sainisumit58@gmail.com>
2016-02-02 08:57:56 +0530

    code committed

---
+++ src/com/doj/spring/car/Car.java
@@ -0,0 +1,11 @@
+package com.doj.spring.car;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Component("testcar") //We use this to remove ambiguity of beans
+public class Car {
+
+}
